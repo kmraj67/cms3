4cb2dad5e9f71cdb8a2b8c6d681cc5ad10400217
Krishan Mohan <krishan.mohan@kiwitech.com>
2017-06-29 14:07:03 +0530

    Chamges

--- db/cms3-2017-05-26.sql
+++ db/cms3-2017-05-26.sql
@@ -98,8 +98,8 @@ CREATE TABLE IF NOT EXISTS `settings` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `key_field` varchar(100) NOT NULL,
   `key_value` varchar(255) NOT NULL,
-  `created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `modified` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `created` datetime NOT NULL ,
+  `modified` datetime NOT NULL ,
   PRIMARY KEY (`id`),
   UNIQUE KEY `key` (`key_field`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=11 ;
